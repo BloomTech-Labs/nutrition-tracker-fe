7a3a8c39ff4da22a9f2e0d4459ec6ca298b02fff
JimmyMcBride <mcbride967@gmail.com>
2019-10-28 22:37:08 -0500

    added firebase sdk and imported firebase and firebase auth in the Auth.js file

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+
 const Auth = () => {
   return (
     <AuthWrapper>

595ab35eecd772d1be401f5d02282c189e78a592
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-03 13:56:19 -0500

    passes root path to Routes to support page routing

--- src/components/Onboarding/index.js
+++ src/components/Onboarding/index.js
@@ -4,23 +4,55 @@ import { Container } from "./styles";
 import TopBar from "./TopBar";
 import DietaryGoal from "./DietaryGoal";
 import Sex from "./Sex";
+import ActivityLevel from "./ActivityLevel";
 import BasicInfo from "./BasicInfo";
 import WeightGoal from "./WeightGoal";
 
 class Onboarding extends React.Component {
-  state = {};
+  state = {
+    userInfo: {
+      sex: "",
+      weight_kg: 0,
+      height_cm: 0,
+      date_of_birth: "",
+      activityLevel: 0,
+      target_weight: 0,
+      target_date: "",
+      target_rate: 0
+    },
+    dietaryGoal: ""
+  };
 
   render() {
     const { path } = this.props.match;
+    console.log("[Onboarding index.js] this.state", this.state);
+
     return (
       <Container direction="column" justify="center" fluid={true}>
-        <TopBar />
-        <Route path={`${path}/dietary-goal`} component={DietaryGoal} />
-        <Route path={`${path}/sex`} component={Sex} />
-        <Route path={`${path}/basic-info`} component={BasicInfo} />
-        <Route path={`${path}/weight-goal`} component={WeightGoal} />
+        <TopBar {...this.props} />
+        <Route
+          path={`${path}/dietary-goal`}
+          render={props => <DietaryGoal {...props} path={path} />}
+        />
+        <Route
+          path={`${path}/sex`}
+          render={props => <Sex {...props} path={path} />}
+        />
+        <Route
+          path={`${path}/activity-level`}
+          render={props => <ActivityLevel {...props} path={path} />}
+        />
+        <Route
+          path={`${path}/basic-info`}
+          render={props => <BasicInfo {...props} path={path} />}
+        />
+        <Route
+          path={`${path}/weight-goal`}
+          render={props => <WeightGoal {...props} path={path} />}
+        />
       </Container>
     );
   }
 }
+
 export default Onboarding;

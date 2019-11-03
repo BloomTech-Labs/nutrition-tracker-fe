97e96ce0637ac73ffd2ff346de38a5cc904922c2
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-03 01:28:40 -0400

    changes order or reusable components

--- src/components/Onboarding/styles.js
+++ src/components/Onboarding/styles.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import theme from "../Global/theme";
 import {
   Button as BS_Button,
@@ -7,32 +7,6 @@ import {
   Row as BS_Row,
   Col as BS_Col
 } from "reactstrap";
-
-export const Input = styled(BS_Input)`
-  font-size: 1.6rem;
-  height: ${({ height }) => (height ? height : "38px")};
-`;
-
-export const PillButton = styled(BS_Button)`
-  border-radius: 50px;
-  width: 100%;
-  height: 45px;
-
-  font-size: 2rem;
-
-  margin-top: 20px;
-`;
-
-export const Header = styled.h1`
-  font-size: 2.8rem;
-`;
-
-export const ButtonContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 
 export const Container = styled(BS_Container)`
   ${props => theme.flex(props.direction, props.justify, props.align)}
@@ -55,4 +29,23 @@ export const Col = styled(BS_Col)`
       props.justify,
       props.align
     )} /* border: 1px solid green; */
+`;
+
+export const Header = styled.h1`
+  font-size: 2.8rem;
+`;
+
+export const Input = styled(BS_Input)`
+  font-size: 1.6rem;
+  height: ${({ height }) => (height ? height : "38px")};
+`;
+
+export const PillButton = styled(BS_Button)`
+  border-radius: 50px;
+  width: 100%;
+  height: 45px;
+
+  font-size: 2rem;
+
+  margin-top: 20px;
 `;

4b52f14bbdc07ab1d504fe517919b212631fded2
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-06 01:34:27 -0500

    adds onboardingReducer to combinReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-import { loginReducer } from "./loginReducer"; // Example of reducer import
+// import { loginReducer } from "./loginReducer"; // Example of reducer import
+import onboardingReducer from "./onboardingReducer";
 
 export default combineReducers({
-  loginReducer // Example of reducer
+  // loginReducer // Example of reducer
+  onboardingReducer
 });

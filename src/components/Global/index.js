e03c9227527b5836765806daa5a8deb3cffef755
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-07 00:31:36 -0500

    adds named exports for global icons

--- src/components/Global/index.js
+++ src/components/Global/index.js
@@ -1,3 +1,4 @@
+// Styled Components
 export { default as AppWrapper } from "./styled-components/AppWrapper";
 export { default as Linkton } from "./styled-components/Linkton";
 export { default as Button } from "./styled-components/Button";
@@ -7,3 +8,11 @@ export { default as ButtonWrapper } from "./styled-components/ButtonWrapper";
 export { default as Container } from "./styled-components/Container";
 export { default as Row } from "./styled-components/Row";
 export { default as Col } from "./styled-components/Col";
+// Icons
+export { default as BackSVG } from "./icons/svg/BackSVG";
+export { default as CalendarSVG } from "./icons/svg/CalendarSVG";
+export { default as RulerSVG } from "./icons/svg/RulerSVG";
+export { default as ScaleSVG } from "./icons/svg/ScaleSVG";
+export { default as EmailSVG } from "./icons/svg/EmailSVG";
+export { default as FacebookSVG } from "./icons/svg/FacebookSVG";
+export { default as GoogleSVG } from "./icons/svg/GoogleSVG";

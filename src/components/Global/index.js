4888243f9d6d72bf823ec34c74a234a8ef41aa0b
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-07 00:05:38 -0500

    creates an export index.js for global styles

--- src/components/Global/index.js
+++ src/components/Global/index.js
@@ -1,41 +1,9 @@
-import styled from "styled-components";
-import theme from "./theme";
-import { Link } from "react-router-dom";
-import { Button as Btn, Form as Frm, Input as Inp } from "reactstrap";
-
-export const AppWrapper = styled.div`
-  min-height: 100vh;
-  text-align: center;
-  background-color: ${theme.g0};
-  margin: 0;
-`;
-
-// Linkton because it's a react router link that looks like a bootstrap button
-export const Linkton = styled(Link)`
-  background: ${theme.secondary};
-  color: ${theme.light};
-  padding: 11px 15px;
-  border-radius: 5px;
-  &:hover {
-    background: ${theme.dark};
-    text-decoration: none;
-    color: ${theme.light};
-  }
-`;
-
-export const Button = styled(Btn)`
-  padding: 7px 15px;
-  border-radius: 5px;
-  &:hover {
-    background: ${theme.dark};
-  }
-`;
-
-export const Form = styled(Frm)``;
-
-export const Input = styled(Inp)``;
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
+export { default as AppWrapper } from "./styled-components/AppWrapper";
+export { default as Linkton } from "./styled-components/Linkton";
+export { default as Button } from "./styled-components/Button";
+export { default as Form } from "./styled-components/Form";
+export { default as Input } from "./styled-components/Input";
+export { default as ButtonWrapper } from "./styled-components/ButtonWrapper";
+export { default as Container } from "./styled-components/Container";
+export { default as Row } from "./styled-components/Row";
+export { default as Col } from "./styled-components/Col";

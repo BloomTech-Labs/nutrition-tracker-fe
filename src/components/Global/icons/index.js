b7bfa6fb6be75d4e8017b80149acfd360a8bf0c1
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-07 00:28:27 -0500

    adds a directory for customizable svg icons

---
+++ src/components/Global/icons/index.js
@@ -0,0 +1,7 @@
+export { default as BackSVG } from "./svg/BackSVG";
+export { default as CalendarSVG } from "./svg/CalendarSVG";
+export { default as RulerSVG } from "./svg/RulerSVG";
+export { default as ScaleSVG } from "./svg/ScaleSVG";
+export { default as EmailSVG } from "./svg/EmailSVG";
+export { default as FacebookSVG } from "./svg/FacebookSVG";
+export { default as GoogleSVG } from "./svg/GoogleSVG";

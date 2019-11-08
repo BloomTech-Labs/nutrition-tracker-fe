f2d1b56d8d257e887bb2d9d84627baed5eb7d913
JimmyMcBride <mcbride967@gmail.com>
2019-11-08 16:53:28 -0600

    cleaned up initial state

--- src/store/reducers/initialState.js
+++ src/store/reducers/initialState.js
@@ -1,16 +1,8 @@
-import firebase from "../components/firebase";
-
-console.log(firebase.auth().currentUser);
-
-const token = localStorage.getItem("token");
-
 // Initial state is in it's own file to be imported in reducer files
 const initialState = {
   error: [],
   items: [],
   item: [],
-  token: token,
-  loading: false,
   getting: false,
   got: false,
   adding: false,
@@ -18,10 +10,7 @@ const initialState = {
   updating: false,
   updated: false,
   deleting: false,
-  deleted: false,
-  loggingIn: false,
-  isLoggedIn: false,
-  loggedOut: token ? false : true
+  deleted: false
 };
 
 export default initialState;

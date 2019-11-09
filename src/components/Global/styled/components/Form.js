ff4de343dd147805ec3886ca82806ece14e73eab
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-09 00:47:44 -0500

    added links to reactstrap doc

--- src/components/Global/styled/components/Form.js
+++ src/components/Global/styled/components/Form.js
@@ -2,3 +2,7 @@ import styled from "styled-components";
 import { Form as BS_Form } from "reactstrap";
 
 export default styled(BS_Form)``;
+
+/*
+  https://reactstrap.github.io/components/form/
+*/
--- src/components/Global/styled/components/Input.js
+++ src/components/Global/styled/components/Input.js
@@ -5,3 +5,9 @@ export default styled(BS_Input)`
   font-size: 1.6rem;
   height: ${({ height }) => (height ? height : "38px")};
 `;
+
+/*
+  Docs:
+    As part of a Form: https://reactstrap.github.io/components/form/
+    As part of an InputGroup: https://reactstrap.github.io/components/input-group/
+*/

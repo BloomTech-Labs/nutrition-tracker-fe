cb20571e69e2fb0e56ad35c6461ba18dd63994cb
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-09 00:51:45 -0500

    added comment

--- src/components/Global/styled/components/AppWrapper.js
+++ src/components/Global/styled/components/AppWrapper.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import theme from "../../theme";
 
+// root container for the application
 export default styled.div`
   min-height: 100vh;
   text-align: center;

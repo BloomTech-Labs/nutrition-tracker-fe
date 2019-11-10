3f2c86b17b20ec20b9e46a6026202e619faa6968
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-10 17:35:03 -0500

    adds align-items prop, propTypes and documentation

--- src/components/Global/styled/components/Row.js
+++ src/components/Global/styled/components/Row.js
@@ -1,32 +1,65 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
-import { Row as BS_Row } from "reactstrap";
+import { Row } from "reactstrap";
 
-export default styled(BS_Row)`
-  /* border: 1px solid blue; */
+const StyledRow = styled(Row)`
+  align-items: ${props => (props.align ? props.align : "stretch")};
   height: ${props => (props.height ? props.height : "auto")};
+
+  /* border: 1px solid blue; */
 `;
 
+export default StyledRow;
+
+StyledRow.propTypes = {
+  // CUSTOM PROPTYPES
+  align: PropTypes.oneOf([
+    "stretch",
+    "flex-start",
+    "flex-end",
+    "center",
+    "baseline"
+  ]),
+  height: PropTypes.string,
+
+  // DEFAULT REACTSTRAP PROPTYPES
+  noGutters: PropTypes.bool,
+  form: PropTypes.bool
+};
+
 /*
-  Rows:
+  ROWS: 
 
     Can only be used in Containers, are the wrappers for Columns, and
     their immediate children must be Columns
 
-  Reactstrap PropTypes:
+  PROP-TYPES: 
+    
+      CUSTOM:
+      
+          NOTE: Rows are flex and have a flex-direction of "row" by default. 
 
-    Row.propTypes = {
-      noGutters: PropTypes.bool,
-      // see https://reactstrap.github.io/components/form Form Grid with Form Row
-      form: PropTypes.bool
-    }
+          align:
+            stretch,        stretch to fill the container (still respect min-width/max-width) (DEFAULT)
+            flex-start,     cross-start margin edge of the items is placed on the cross-start line
+            flex-end,       cross-end margin edge of the items is placed on the cross-end line
+            center,         items are centered in the cross-axis
+            baseline,       items are aligned such as their baselines align
 
-    noGutters=true - removes the default margin from rows and default padding from 
-                    columns
-  
-  Docs:
+      REACTSTRAP: 
 
-    https://reactstrap.github.io/components/layout/
-    https://getbootstrap.com/docs/4.1/layout/grid/
+          noGutters:        
+            true,           removes the default margin from rows and default padding from columns
+
+          https://reactstrap.github.io/components/layout/
+
+  DOCS: 
+    ReactStrap Layout:
+      https://reactstrap.github.io/components/layout/
+    BootStrap Layout/Grid:
+      https://getbootstrap.com/docs/4.3/layout/grid/
+    Flexbox:
+      https://css-tricks.com/snippets/css/a-guide-to-flexbox/
 
 */

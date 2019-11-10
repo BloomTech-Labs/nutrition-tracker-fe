a76ad93fb664e17c7fd67b53b91f57b5f6931380
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-10 17:05:29 -0500

    comments out unused global components

--- src/components/Global/styled/index.js
+++ src/components/Global/styled/index.js
@@ -1,13 +1,14 @@
 // Styled Components
 export { default as AppWrapper } from "./components/AppWrapper";
-export { default as Linkton } from "./components/Linkton";
-export { default as Button } from "./components/Button";
-export { default as Form } from "./components/Form";
+// DELETE export { default as Linkton } from "./components/Linkton";
+// DELETE export { default as Button } from "./components/Button";
+// DELETE export { default as Form } from "./components/Form";
 export { default as Input } from "./components/Input";
-export { default as ButtonWrapper } from "./components/ButtonWrapper";
+// DELETE export { default as ButtonWrapper } from "./components/ButtonWrapper";
 export { default as Container } from "./components/Container";
 export { default as Row } from "./components/Row";
 export { default as Col } from "./components/Col";
 export { default as PillButton } from "./components/PillButton";
-export { default as SignInButton } from "./components/SignInButton";
-export { default as Header } from "./components/Header";
+// DELETE export { default as SignInButton } from "./components/SignInButton";
+export { default as H1 } from "./components/H1";
+export { default as H2 } from "./components/H2";

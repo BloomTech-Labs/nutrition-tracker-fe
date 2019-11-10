34273ac1208362c0b14ab69a05e4c3030a2827c7
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-10 17:40:17 -0500

    updates exports to remove unused global components

--- src/components/Global/styled/index.js
+++ src/components/Global/styled/index.js
@@ -1,10 +1,9 @@
 // Styled Components
 export { default as AppWrapper } from "./components/AppWrapper";
-export { default as Linkton } from "./components/Linkton";
-export { default as Button } from "./components/Button";
-export { default as Form } from "./components/Form";
-export { default as Input } from "./components/Input";
-export { default as ButtonWrapper } from "./components/ButtonWrapper";
 export { default as Container } from "./components/Container";
 export { default as Row } from "./components/Row";
 export { default as Col } from "./components/Col";
+export { default as Input } from "./components/Input";
+export { default as PillButton } from "./components/PillButton";
+export { default as H1 } from "./components/H1";
+export { default as H2 } from "./components/H2";

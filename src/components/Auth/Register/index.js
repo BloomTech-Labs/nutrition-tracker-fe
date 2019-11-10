ed01307345390241e2a9f1f6893ec736724b6338
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-10 17:40:41 -0500

    comments out main content of page

--- src/components/Auth/Register/index.js
+++ src/components/Auth/Register/index.js
@@ -1,35 +1,36 @@
 import React from "react";
 
-import styled from "styled-components";
+// import styled from "styled-components";
 
-import { Button, Form, Input, ButtonWrapper } from "../../Global/styled";
-import { Linkton } from "../../Global/styled";
+// import { Button, Form, Input, ButtonWrapper } from "../../Global/styled";
+// import { Linkton } from "../../Global/styled";
 
 const Register = () => {
   return (
-    <RegisterWrapper>
-      <h2>Register</h2>
-      <Form onSubmit={e => e.preventDefault() && false}>
-        <Input name="name" placeholder="Username" type="text" />
+    <h1>Register Page</h1>
+    // <RegisterWrapper>
+    //   <h2>Register</h2>
+    //   <Form onSubmit={e => e.preventDefault() && false}>
+    //     <Input name="name" placeholder="Username" type="text" />
 
-        <Input name="email" type="text" placeholder="Email" />
+    //     <Input name="email" type="text" placeholder="Email" />
 
-        <Input name="password" type="password" placeholder="Password" />
+    //     <Input name="password" type="password" placeholder="Password" />
 
-        <ButtonWrapper>
-          <Button>Register</Button>
+    //     <ButtonWrapper>
+    //       <Button>Register</Button>
 
-          <Linkton to="/">Go back to Login</Linkton>
-        </ButtonWrapper>
-      </Form>
-    </RegisterWrapper>
+    //       <Linkton to="/">Go back to Login</Linkton>
+    //     </ButtonWrapper>
+    //   </Form>
+    // </RegisterWrapper>
   );
 };
 
-const RegisterWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+// const RegisterWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
 
 export default Register;

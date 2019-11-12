10176e4ec3a80c711caf7a9650f32da1ad5e167f
Chaz Rowe <chazr_88@hotmail.com>
2019-11-12 02:17:04 -0500

    pulled updated staging branch into this one

--- src/components/Settings/components/height.js
+++ src/components/Settings/components/height.js
@@ -30,7 +30,7 @@ const Height = props => {
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          culpa qui officia deserunt mollit anim id est lborum.
         </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={toggle}>

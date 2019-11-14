3a400779a8116bb7b84af3f814a45d813f38fe84
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-11-13 18:17:18 -0800

    Added enzyme dependency, will need it to effectively test React Router components

--- src/components/Auth/Login/test/index.unit.spec.js
+++ src/components/Auth/Login/test/index.unit.spec.js
@@ -1,3 +1,14 @@
-test("test", ()=>{
-    expect(2+2).toBe(4);
-})
+import React from 'react';
+import Login from '../index';
+import renderer from 'react-test-renderer';
+import expectExport from 'expect';
+import { Route } from "react-router-dom";
+
+test('Render Login component', () => {
+    const component = renderer.create(
+        <Route path="/login" exact component={Login} />
+    );
+
+    let tree = component.toJSON();
+    expectExport(tree).toMatchSnapshot();
+});

a93bbaf8768da54fb4b39eac586ad0cc0adb241a
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-11-18 19:09:19 -0800

    Implementing mock redux store

---
+++ src/components/FoodItem/test/index.unit.spec.js
@@ -0,0 +1,49 @@
+import { mount, configure } from "enzyme";
+import React from "react";
+import { Provider } from "react-redux";
+import SearchForm from "../searchForm";
+import Adapter from "enzyme-adapter-react-16";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+// import your action
+import FETCH_SUCCESS from "../../../store/actions/foodItemAction";
+// function that emulates a successful response from the api
+function fetchData() {
+  return dispatch => {
+    return { type: FETCH_SUCCESS };
+  };
+}
+
+// TODO: DRY store so that we don't have to recreate this
+//mock store stuff on every tested component
+const initialState = {};
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+let store;
+
+configure({ adapter: new Adapter() });
+
+describe("<SearchForm />", () => {
+  beforeEach(() => {
+    store = mockStore(initialState);
+    store.dispatch(fetchData());
+  });
+
+  test("simulate click event", async () => {
+    // TODO: need to mock searchFoodItems module and pass it to searchFoodItems prop
+    const wrapper = await mount(
+      <Provider store={store}>
+        <SearchForm searchFoodItems={() => {}} />
+      </Provider>
+    );
+    
+    // .simulate() gets given prop and calls it, for example onChange is being called here
+    // the second arg to .simulate() is an event object that we want passed down!
+    await wrapper.find("input#search_term").simulate("change", { target: { value: "bacon" } });
+
+    // we'd be expecting the value prop to be "bacon" after we've simulated a change
+    expect(wrapper.find("input#search_term").prop("value")).toBe("bacon");
+    
+  });
+});

01d2a65492875a9767c7fd4436716df877884c0a
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-20 20:24:14 -0500

    exports the global h3

--- src/components/Global/styled/index.js
+++ src/components/Global/styled/index.js
@@ -7,3 +7,4 @@ export { default as Input } from "./components/Input";
 export { default as PillButton } from "./components/PillButton";
 export { default as H1 } from "./components/H1";
 export { default as H2 } from "./components/H2";
+export { default as H3 } from "./components/H3";

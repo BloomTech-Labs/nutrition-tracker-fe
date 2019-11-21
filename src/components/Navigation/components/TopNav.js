f8c58b343d6b28e9f5fd974a258adc9c66a74d22
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-20 22:57:46 -0500

    creates top nav bar

---
+++ src/components/Navigation/components/TopNav.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import styled from "styled-components";
+import {
+  Navbar as BS_NavBar,
+  NavbarBrand as BS_NavbarBrand,
+  Button as BS_Button
+} from "reactstrap";
+import { BackSVG } from "../../Global/icons";
+import theme from "../../Global/theme";
+
+const TopNav = props => {
+  const {
+    pageTitle,
+    titleColor,
+    topNavColor,
+    buttonColor,
+    iconColor,
+    display,
+    history
+  } = props;
+
+  return (
+    <NavContainer display={display}>
+      <Navbar topNavColor={topNavColor}>
+        <Button buttonColor={buttonColor} onClick={() => history.goBack()}>
+          <BackSVG fill={iconColor} />
+        </Button>
+        <NavbarBrand titleColor={titleColor}>
+          {pageTitle}
+        </NavbarBrand>
+      </Navbar>
+    </NavContainer>
+  );
+};
+
+const NavContainer = styled.div`
+  display: ${props => (props.display ? "block" : "none")};
+`;
+
+const Navbar = styled(BS_NavBar)`
+  ${theme.mixin.flex("row", "flex-start", "center")};
+
+  padding: 0 3px;
+  height: 50px;
+
+  background-color: ${props => props.topNavColor};
+`;
+
+const Button = styled(BS_Button)`
+  border: none;
+  background-color: ${props => props.buttonColor};
+`;
+
+const NavbarBrand = styled(BS_NavbarBrand)`
+  color: ${props => props.titleColor} !important;
+  margin-left: 10px;
+  font-size: 1.6rem;
+`;
+
+export default withRouter(TopNav);

7b2042fe464616c5c18ed735128a39a8e66bc52e
Alejandro Mejia <aalejandromr@AlejandrosAir2.lan1>
2019-11-21 23:30:20 -0500

    Adds Search Page and Search Results testing

--- src/components/FoodItem/searchResults.js
+++ src/components/FoodItem/searchResults.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Table } from "reactstrap";
+import { TBody } from "../Global/styled/";
 
 class SearchResults extends React.Component {
   render() {
-    return this.props.items.map((foodItem, key) => {
-      return (
-        <tr
-          key={key}
-          onClick={() => this.props.handleGetFoodItem(foodItem.food_id)}
-        >
-          <th scope="row"> {foodItem.food_name} </th>
-          <td>{foodItem.standard_amount}</td>
-          <td>{foodItem.calories} cal</td>
-        </tr>
-      );
-    });
+    return this.props.items ? (
+      <Table responsive hover size="lg">
+        <TBody>
+          {this.props.items.map((foodItem, key) => {
+            return (
+              <tr
+                id="search_results"
+                key={key}
+                onClick={() => this.props.handleGetFoodItem(foodItem.food_id)}
+              >
+                <th scope="row"> {foodItem.food_name} </th>
+                <td>{foodItem.standard_amount}</td>
+                <td>{foodItem.calories} cal</td>
+              </tr>
+            );
+          })}
+        </TBody>
+      </Table>
+    ) : (
+      <div />
+    );
   }
 }
 
@@ -24,4 +35,5 @@ const mapStateToProps = state => {
   };
 };
 
+export { SearchResults };
 export default connect(mapStateToProps, {})(SearchResults);
--- src/components/FoodItem/test/index.unit.spec.js
+++ src/components/FoodItem/test/index.unit.spec.js
@@ -1,24 +1,125 @@
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import React from "react";
 import { Provider } from "react-redux";
 import SearchForm from "../searchForm";
+import SearchResults from "../searchResults";
 
+let store;
+let wrapper;
+const mockItems = [
+  {
+    food_name: "Pupusa",
+    standard_amount: 1,
+    calories: 2.99
+  },
+  {
+    food_name: "Tacos",
+    standard_amount: 4,
+    calories: 14.2
+  },
+  {
+    food_name: "Cheese Hamburguer",
+    standard_amountt: 1,
+    calories: 2093.1
+  }
+];
 describe("<SearchForm />", () => {
-  test("simulate click event", async () => {
-    // TODO: need to mock searchFoodItems module and pass it to searchFoodItems prop
-    const wrapper = await mount(
-      <Provider store={_bigMockStore_()}>
+  beforeEach(async () => {
+    store = global._bigMockStore_();
+    wrapper = await mount(
+      <Provider store={store}>
         <SearchForm />
       </Provider>
     );
+  });
+
+  test("simulate click event", () => {
+    // TODO: need to mock searchFoodItems module and pass it to searchFoodItems prop
 
     // .simulate() gets given prop and calls it, for example onChange is being called here
     // the second arg to .simulate() is an event object that we want passed down!
-    await wrapper
+    wrapper
       .find("input#search_term")
       .simulate("change", { target: { value: "bacon" } });
 
     // we'd be expecting the value prop to be "bacon" after we've simulated a change
     expect(wrapper.find("input#search_term").prop("value")).toBe("bacon");
+  });
+
+  test("test <SearchForm /> renders a search bar", () => {
+    expect(wrapper.find("input#search_term")).toHaveLength(1);
+  });
+});
+
+describe("<SearchResults/>", () => {
+  beforeEach(async () => {
+    store = global._bigMockStore_({
+      foodItemsReducer: {
+        items: []
+      }
+    });
+  });
+
+  test("Renders", async () => {
+    const searchWrapper = await mount(
+      <Provider store={store}>
+        <SearchResults />
+      </Provider>
+    );
+    expect(searchWrapper).toMatchSnapshot();
+  });
+
+  test("Renders Results when availables", async () => {
+    store = global._bigMockStore_({
+      foodItemsReducer: {
+        items: mockItems
+      }
+    });
+
+    const searchWrapper = await mount(
+      <Provider store={store}>
+        <SearchResults />
+      </Provider>
+    );
+    expect(searchWrapper.find("tr")).not.toHaveLength(0);
+  });
+
+  test("Doesn't break without items", async () => {
+    store = global._bigMockStore_({
+      foodItemsReducer: {}
+    });
+
+    const searchWrapper = await mount(
+      <Provider store={store}>
+        <SearchResults />
+      </Provider>
+    );
+    expect(searchWrapper.find("tr")).toHaveLength(0);
+    expect(searchWrapper.find("div")).toHaveLength(1);
+  });
+
+  test("Click event calls store once when triggered", async () => {
+    store = global._bigMockStore_({
+      foodItemsReducer: {
+        items: mockItems
+      }
+    });
+
+    const handleGetFoodItem = jest.fn();
+
+    const searchWrapper = await mount(
+      <Provider store={store}>
+        <SearchResults handleGetFoodItem={handleGetFoodItem} />
+      </Provider>
+    );
+
+    searchWrapper
+      .find("tr")
+      .at(0)
+      .simulate("click");
+
+    // We have three objects on the mock store, find will find the 3 of them and simulate click
+    // therefore store.dispatch will has been called once per child rendered
+    expect(handleGetFoodItem.mock.calls.length).toBe(1);
   });
 });

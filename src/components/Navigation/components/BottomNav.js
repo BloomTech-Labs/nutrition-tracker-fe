3e6d63e9f16be298375bee2f533122c8f1b6582b
Paul Abernathy <paul.abernathy7@gmail.com>
2019-11-21 22:21:02 -0500

    updated Icon names

--- src/components/Navigation/components/BottomNav.js
+++ src/components/Navigation/components/BottomNav.js
@@ -5,45 +5,48 @@ import {
   NavItem as BS_NavItem,
   NavLink
 } from 'reactstrap';
-import { DailyLogIcon, RecipeIcon, ProgressIcon, SettingsIcon } from '../../Global/icons';
+import { DailyLogSVG, RecipeSVG, ProgressSVG, SettingsSVG } from '../../Global/icons';
 import theme from '../../Global/theme';
-
+​
 const BottomNav = ({display}) => {
   return (
     <NavContainer display={display}>
       <NavBar>
         <NavItem>
-          <NavLink href="/daily-log"><DailyLogIcon /></NavLink>
+          <NavLink href="/daily-log"><DailyLogSVG /></NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="/recipes"><RecipeIcon /></NavLink>
+          <NavLink href="/recipes"><RecipeSVG /></NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="/reports"><ProgressIcon /></NavLink>
+          <NavLink href="/reports"><ProgressSVG /></NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="/settings"><SettingsIcon /></NavLink>
+          <NavLink href="/settings"><SettingsSVG /></NavLink>
         </NavItem>
       </NavBar>
     </NavContainer>
   )
 }
-
+​
 const NavContainer = styled.div`
   display: ${props => props.display ? "block" : "none"};
 `;
-
+​
 const NavBar = styled(BS_NavBar)`
   ${theme.mixin.flex("row", "space-between", "center")};
-  padding: 0;
+​
   height: 50px;
+  padding: 0;
+​
   background-color: black;
 `;
-
+​
 const NavItem = styled(BS_NavItem)`
   ${theme.mixin.flex("row", "center", "center")};
+​
   width: 25%;
   height: 100%;
 `;
-
+​
 export default BottomNav;

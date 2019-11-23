352b6246d8fd05ddde0fd48bf541ebfa552e48a3
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-23 16:28:21 -0500

    adds navigation to DailyLog

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AppWrapper } from "./components/Global/styled";
 
 // Set up routes
 import { Route } from "react-router-dom";
+import withNavigation from "./components/Navigation/withNavigation"
 
 // importing component pages for routes
 import DailyLog from "./components/DailyLog";
@@ -17,11 +18,15 @@ import Onboarding from "./components/Onboarding";
 import PrivateRoute from "./components/PrivateRoute";
 // import RequireAuth from "./components/Auth";
 
+const DailyLogWithNav = withNavigation({
+  displayTop: false
+})(DailyLog);
+
 class App extends Component {
   render() {
     return (
       <AppWrapper>
-        <PrivateRoute exact path="/" component={DailyLog} />
+        <PrivateRoute exact path="/" component={DailyLogWithNav} />
         <Route path="/landing" component={LandingPage} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />

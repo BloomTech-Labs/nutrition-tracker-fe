7cf63b30d1282572499e3e763a36f0dd3ff0ef33
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-23 16:31:14 -0500

    imports new daily log components

--- src/components/DailyLog/index.js
+++ src/components/DailyLog/index.js
@@ -1,28 +1,55 @@
 import React from "react";
-import { Container, Row, Col, H2, H3 } from "../Global/styled";
+import styled from "styled-components";
+import { Container, Row, Col } from "../Global/styled";
+import { Table as BS_Table } from "reactstrap";
 import CaloricBudget from "./components/CaloricBudget";
+import MacroBudgets from "./components/MacroBudgets";
+import theme from "../Global/theme";
+import Pagination from "./components/Pagination";
 
-function DailyLog() {
+function DailyLog({ height }) {
   return (
-    <Container fluid>
+    <Container fluid height={height}>
+      <CaloricBudget consumed={1280} total={2140} />
+      <MacroBudgets
+        fatsConsumed={52}
+        fatsTotal={124}
+        carbsConsumed={20}
+        carbsTotal={45}
+        protienConsumed={48}
+        protienTotal={80}
+      />
+      <Pagination />
       <Row>
-        <Col height="100px" align="center">
-          <CaloricBudget consumed={1280} total={2140}/>
-        </Col>
-      </Row>
-      <Row>
-        <Col justify="center">
-          <H3>Fats</H3>
-        </Col>
-        <Col justify="center">
-          <H3>Carbs</H3>
-        </Col>
-        <Col justify="center">
-          <H3>Protein</H3>
+        <Col>
+          <Table>
+            <tbody>
+              <tr>
+                <th>8:00AM</th>
+                <td>Egg</td>
+                <td>medium</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td style={{ border: "none" }} />
+                <td>White Bread</td>
+                <td>slice/s</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td style={{ border: "none" }} />
+                <td>Orange Juice</td>
+                <td>large glass</td>
+                <td>1</td>
+              </tr>
+            </tbody>
+          </Table>
         </Col>
       </Row>
     </Container>
   );
 }
+
+const Table = styled(BS_Table)``;
 
 export default DailyLog;

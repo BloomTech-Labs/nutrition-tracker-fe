fc121666c2688a3757cafbeba19a979140c93bf8
Paul Abernathy <paul.abernathy7@gmail.com>
2019-11-30 13:18:22 -0500

    added component

--- src/components/Settings/components/macro_targets.js
+++ src/components/Settings/components/macro_targets.js
@@ -1,3 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import { Col, Row, Container } from "../../Global/styled";
+
+class MacroTargets extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col></Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default MacroTargets;

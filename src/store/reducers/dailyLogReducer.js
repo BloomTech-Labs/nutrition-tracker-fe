bee309e4e173bc6232e7fde13ac98fbceb31a7a1
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-30 18:38:21 -0500

    creates a reducer for DailyLog

---
+++ src/store/reducers/dailyLogReducer.js
@@ -0,0 +1,98 @@
+import {
+  FETCH_NUTRITION_BUDGETS_START,
+  FETCH_NUTRITION_BUDGETS_SUCCESS,
+  FETCH_NUTRITION_BUDGETS_FAILURE,
+  FETCH_DAILY_LOG_START,
+  FETCH_DAILY_LOG_SUCCESS,
+  FETCH_DAILY_LOG_FAILURE
+} from "../actions/dailyLogActions";
+
+const initialState = {
+  budgets: {
+    caloricBudget: 0,
+    fatBudget: 0,
+    carbBudget: 0,
+    proteinBudget: 0
+  },
+  consumed: {
+    caloriesConsumed: 0,
+    fatsConsumed: 0,
+    carbsConsumed: 0,
+    proteinConsumed: 0
+  },
+  dailyLog: null,
+  fetchBudgetStart: false,
+  fetchBudgetSuccess: false,
+  fetchBudgetFailure: false,
+  fetchDailyLogStart: false,
+  fetchDailyLogSuccess: false,
+  fetchDailyLogError: false,
+};
+
+export const dailyLogReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_NUTRITION_BUDGETS_START: {
+      return {
+        ...state,
+        fetchBudgetStart: true,
+        fetchBudgetSuccess: false,
+        fetchBudgetFailure: false,
+      };
+    }
+
+    case FETCH_NUTRITION_BUDGETS_SUCCESS: {
+      return {
+        ...state,
+        fetchBudgetStart: false,
+        fetchBudgetSuccess: true,
+        fetchBudgetFailure: false,
+        budgets: action.payload
+      };
+    }
+
+    case FETCH_NUTRITION_BUDGETS_FAILURE: {
+      return {
+        ...state,
+        fetchBudgetStart: false,
+        fetchBudgetSuccess: false,
+        fetchBudgetFailure: true,
+      };
+    }
+
+    case FETCH_DAILY_LOG_START: {
+      return {
+        ...state,
+        fetchDailyLogStart: true,
+        fetchDailyLogSuccess: false,
+        fetchDailyLogError: false,
+        consumed: initialState.consumed,
+        dailyLog: initialState.dailyLog
+      };
+    }
+
+    case FETCH_DAILY_LOG_SUCCESS: {
+      return {
+        ...state,
+        fetchDailyLogStart: false,
+        fetchDailyLogSuccess: true,
+        fetchDailyLogError: false,
+        consumed: action.payload.consumed,
+        dailyLog: action.payload.dailyLog
+      };
+    }
+
+    case FETCH_DAILY_LOG_FAILURE: {
+      return {
+        ...state,
+        fetchDailyLogStart: false,
+        fetchDailyLogSuccess: false,
+        fetchDailyLogError: true,
+        consumed: initialState.consumed,
+        dailyLog: initialState.dailyLog
+      };
+    }
+
+    default:
+      return state;
+  }
+};

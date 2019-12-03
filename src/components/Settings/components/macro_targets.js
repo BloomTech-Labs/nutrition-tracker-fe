8784f94590b12811603ab98cb4b17cea5aee0cac
Paul Abernathy <paul.abernathy7@gmail.com>
2019-12-02 22:59:30 -0500

    refactored to functional component

--- src/components/Settings/components/macro_targets.js
+++ src/components/Settings/components/macro_targets.js
@@ -1,17 +1,68 @@
-import React from "react";
-import styled from "styled-components";
-import { Col, Row, Container } from "../../Global/styled";
+import React, { useState, useEffect } from "react";
+import { ListStyle } from "../styles";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ListGroupItem,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 
-class MacroTargets extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col></Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+const Macros = props => {
+  const [modal, setModal] = useState(false);
 
-export default MacroTargets;
+  const [fat, setFat] = useState("");
+  const [carbs, setCarbs] = useState("");
+  const [protein, setProtein] = useState("");
+
+  useEffect(() => {
+    setFat(props.data.fat_ratio);
+  }, [props.data.fat_ratio]);
+
+  const toggle = () => setModal(!modal);
+  return (
+    <div>
+      <ListGroupItem onClick={toggle} style={ListStyle}>
+        <div>Macronutrient Targets</div>
+      </ListGroupItem>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Macronutrient Targets</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="email">Macronutrient Targets</Label>
+              <Input
+                type="text"
+                name="email"
+                id="email"
+                value={email || ""}
+                onChange={e => setMac(e.target.value)}
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => {
+              toggle();
+              props.updateUser({ fat_ratio, carbs_ratio, protein_ratio });
+            }}
+          >
+            Update
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default Macros;
--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import { connect } from "react-redux";
-import { Container as Global_Container, Row, Col} from "../Global/styled";
-import {ListStyle, HeadingStyle} from "./styles";
-import {getUserInfo, updateUserInfo} from "../../store/actions/settingsActions";
-import {ListGroup, ListGroupItem } from "reactstrap";
+import { Container as Global_Container, Row, Col } from "../Global/styled";
+import { ListStyle, HeadingStyle } from "./styles";
+import {
+  getUserInfo,
+  updateUserInfo
+} from "../../store/actions/settingsActions";
+import { ListGroup, ListGroupItem } from "reactstrap";
 import Height from "./components/height";
 import Dob from "./components/dob";
 import Email from "./components/email";
@@ -14,38 +17,37 @@ import ActivityLevel from "./components/activity_level";
 import CurrentWeight from "./components/current_weight";
 
 class Settings extends React.Component {
-
   componentDidMount() {
-    const id = 1
+    const id = 1;
     //Calls action to get specific user
     this.props.getUserInfo(id);
   }
 
-  updateUser = (update) => {
+  updateUser = update => {
     console.log("[update]", update);
-    this.props.updateUserInfo(update)
-  } 
+    this.props.updateUserInfo(update);
+  };
 
   render() {
-    console.log(this.user)
+    console.log(this.user);
     return (
       <Container height={this.props.height}>
-          <ListGroup>
+        <ListGroup>
           <ListGroupItem style={HeadingStyle}>Profile</ListGroupItem>
           <Height updateUser={this.updateUser} data={this.props.userInfo} />
           <CurrentWeight data={this.props.userInfo} />
           <Dob updateUser={this.updateUser} data={this.props.userInfo} />
           <Gender updateUser={this.updateUser} data={this.props.userInfo} />
-          <ActivityLevel  />
+          <ActivityLevel />
           <ListGroupItem style={HeadingStyle}>Nutrition</ListGroupItem>
-          <ListGroupItem style={ListStyle}>
+          {/* <ListGroupItem style={ListStyle}>
             MacroNutrient Targets
-          </ListGroupItem>
-          <ListGroupItem style={ListStyle}>Weight Goal</ListGroupItem>
+          </ListGroupItem> */}
+          {/* <ListGroupItem style={ListStyle}>Weight Goal</ListGroupItem> */}
           <ListGroupItem style={HeadingStyle}>Account Settings</ListGroupItem>
           <ListGroupItem style={ListStyle}>Logout</ListGroupItem>
           <Email updateUser={this.updateUser} data={this.props.userInfo} />
-          <Password  />
+          <Password />
         </ListGroup>
       </Container>
     );
@@ -58,9 +60,11 @@ const Container = styled(Global_Container)`
 `;
 
 const mapStateToProps = state => {
-  return{
+  return {
     userInfo: state.updateUserInfo
-  }
+  };
 };
 
-export default connect(mapStateToProps, { getUserInfo, updateUserInfo })(Settings);
+export default connect(mapStateToProps, { getUserInfo, updateUserInfo })(
+  Settings
+);

2fa7a240aee6a3c0a1f1a030ded701cd42528c18
JimmyMcBride <mcbride967@gmail.com>
2019-12-04 21:25:38 -0600

    commented out toast until error gets fixed

--- src/App.js
+++ src/App.js
@@ -20,28 +20,48 @@ import PrivateRoute from "./components/PrivateRoute";
 
 import withNavigation from "./components/Navigation/withNavigation";
 
-// imports for toast wrapper 
-import { ToastProvider } from "react-toast-notifications";
+// imports for toast wrapper
+// import { ToastProvider } from "react-toast-notifications";
 
 const SettingsWithNav = withNavigation({
   pageTitle: "Settings"
 })(Settings);
 
+const OnboardingWithNav = withNavigation({
+  iconColor: "black",
+  topNavColor: "white",
+  displayBottom: false
+})(Onboarding);
 
+const LoginWithNav = withNavigation({
+  iconColor: "black",
+  topNavColor: "white",
+  displayBottom: false
+})(Login);
+
+const RegisterWithNav = withNavigation({
+  iconColor: "black",
+  topNavColor: "white",
+  displayBottom: false
+})(Register);
+
+const DailyLogWithNav = withNavigation({
+  pageTitle: "Daily Log"
+})(DailyLog);
 
 class App extends Component {
   render() {
     return (
-      <ToastProvider number="5000"> 
+      // <ToastProvider number="5000">
       <AppWrapper>
-        <PrivateRoute exact path="/" component={DailyLog} />
+        <PrivateRoute exact path="/" component={DailyLogWithNav} />
         <Route path="/landing" component={LandingPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/onboarding" component={Onboarding} />
-        <PrivateRoute path="/settings" component={SettingsWithNav} />
+        <Route path="/login" component={LoginWithNav} />
+        <Route path="/register" component={RegisterWithNav} />
+        <Route path="/onboarding" component={OnboardingWithNav} />
+        <Route path="/settings" component={SettingsWithNav} />
       </AppWrapper>
-      </ToastProvider>
+      // </ToastProvider>
     );
   }
 }

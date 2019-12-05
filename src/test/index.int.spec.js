c8e7fd2cceaefcaef56ac995d753edbe458e8cff
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-12-04 19:09:33 -0800

    <App /> component tests WIP

--- src/test/index.int.spec.js
+++ src/test/index.int.spec.js
@@ -1,3 +1,16 @@
-test("test", ()=>{
-    expect(2+2).toBe(4);
+import App from "../App";
+import { mount, shallow } from "enzyme";
+import React from "react";
+import { Provider } from "react-redux";
+
+const store = global._bigMockStore_();
+
+describe("<App />", () => {
+    test("Main <App /> component renders", () => {
+        const wrapper = shallow(
+            <Provider store={store} >
+        <App />
+        </Provider>);
+        console.log(wrapper.debug());
+    })
 })

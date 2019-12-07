58d9ae3d5c28e8fcaf5e1aa36abf7f4669df7c8b
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-07 15:27:45 -0500

    refactors Datawheel to be more "global"

--- src/components/Global/DataWheel/index.js
+++ src/components/Global/DataWheel/index.js
@@ -1,20 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment-timezone";
 import theme from "../theme";
 import styled from "styled-components";
 import { Doughnut } from "react-chartjs-2";
 import { H3, H4 } from "../styled";
+import { fetchNutritionBudgets, fetchDailyLog } from "../../../store/actions/dailyLogActions";
 
 const DataWheel = props => {
+  const dispatch = useDispatch();
+  const firebaseID = useSelector(state => state.firebase.auth.uid);
   const {
-    macroName,
-    total,
+    budgets,
     consumed,
-    percentTotal,
-    dataColors,
-    borderColor,
-    hoverBorderColor,
-    hoverBackgroundColor
-  } = props;
+    fetchBudgetFailure,
+    fetchBudgetStart
+  } = useSelector(state => state.dailyLog);
+  const [macroData, setMacroData] = useState({});
+  const [dataFetched, setDataFetched] = useState(false);
+
+  useEffect(
+    () => {
+      if (firebaseID) {
+        dispatch(fetchNutritionBudgets(firebaseID))
+      }
+    },
+    [firebaseID, dispatch]
+  );
+
+  useEffect(
+    () => {
+      if (fetchBudgetFailure === false && fetchBudgetStart === false) {
+        switch (props.macroName) {
+          case "Fats":
+            setMacroData({
+              consumed: consumed.fatsConsumed,
+              total: budgets.fatBudget,
+              percentTotal: Math.round(
+                consumed.fatsConsumed / budgets.fatBudget * 100
+              ),
+              colors: {
+                dataColors: "#FFE9AD, #FFE9AD33",
+                borderColor: "#E4D099",
+                hoverBorderColor: "#D5C28F",
+                hoverBackgroundColor: "#FED872"
+              }
+            });
+            break;
+          case "Carbs":
+            setMacroData({
+              consumed: consumed.carbsConsumed,
+              total: budgets.carbBudget,
+              percentTotal: Math.round(
+                consumed.carbsConsumed / budgets.carbBudget * 100
+              ),
+              colors: {
+                dataColors: "#A1BFDF, #A1BFDF33",
+                borderColor: "#829BB6",
+                hoverBorderColor: "#778EA6",
+                hoverBackgroundColor: "#5F98E5"
+              }
+            });
+            break;
+          case "Protein":
+            setMacroData({
+              consumed: consumed.proteinConsumed,
+              total: budgets.proteinBudget,
+              percentTotal: Math.round(
+                consumed.proteinConsumed / budgets.proteinBudget * 100
+              ),
+              colors: {
+                dataColors: "#F5C6CB, #F5C6CB33",
+                borderColor: "#BC9599",
+                hoverBorderColor: "#A68588",
+                hoverBackgroundColor: "#FD7888"
+              }
+            });
+            break;
+          default:
+            setMacroData({});
+        }
+        setDataFetched(true);
+      } else {
+        setDataFetched(false)
+      }
+    },
+    [
+      fetchBudgetStart,
+      fetchBudgetFailure,
+      props.macroName,
+      consumed,
+      budgets,
+      dispatch
+    ]
+  );
 
   const options = {
     cutoutPercentage: 75,
@@ -34,30 +113,36 @@ const DataWheel = props => {
     },
     maintainAspectRatio: false
   };
-
+  if (!dataFetched) {
+    return <h1>Loading...</h1>;
+  }
+  // console.log("[macroData]", macroData);
   return (
     <BudgetContainer>
       <Doughnut
         options={options}
         data={dataGenerator(
-          [percentTotal, 100 - percentTotal],
-          dataColors,
-          borderColor,
-          hoverBackgroundColor,
-          hoverBorderColor
+          [macroData.percentTotal, 100 - macroData.percentTotal],
+          macroData.colors.dataColors,
+          macroData.colors.borderColor,
+          macroData.colors.hoverBackgroundColor,
+          macroData.colors.hoverBorderColor
         )}
       />
-      <Budgets dividerColor={borderColor} percentTotal={percentTotal}>
+      <Budgets
+        dividerColor={macroData.colors.borderColor}
+        percentTotal={macroData.percentTotal}
+      >
         <H3>
-          {consumed}g
+          {macroData.consumed}g
         </H3>
         <div />
         <H4>
-          {total}g
+          {macroData.total}g
         </H4>
       </Budgets>
       <BudgetHeader>
-        {macroName}
+        {props.macroName}
       </BudgetHeader>
     </BudgetContainer>
   );
@@ -108,6 +193,8 @@ const BudgetContainer = styled.div`
   max-width: 150px;
 
   padding: 0 5px;
+
+  /* border: 1px solid black; */
 
   canvas {
     z-index: 1;

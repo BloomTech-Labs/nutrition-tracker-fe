1571277f27b8a90de9cebdd32f581760acd5252c
Paul Abernathy <paul.abernathy7@gmail.com>
2019-12-07 16:25:42 -0500

    added weight goal js

---
+++ src/components/Settings/components/weight_goal.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { ListStyle } from "../styles";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ListGroupItem,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+
+export default WeightGoal;

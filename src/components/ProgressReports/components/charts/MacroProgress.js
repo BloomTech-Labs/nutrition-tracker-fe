7e0d8c555a7cfc739720b7da7c07707a1768cc3b
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-15 14:40:09 -0500

    updates styles to macro pie-chart

--- src/components/ProgressReports/components/charts/MacroProgress.js
+++ src/components/ProgressReports/components/charts/MacroProgress.js
@@ -4,41 +4,37 @@ import { Pie } from "react-chartjs-2";
 const MacroProgress = () => {
   const data = () => {
     return {
-      labels: ["Carbs", "Fat", "Protein"],
+      labels: ["Fat", "Carbs", "Protein"],
       datasets: [
         {
-          // label: "Target",
-          data: [10, 70, 20],
-          // fill: false,
-          backgroundColor: ["royalblue", "gold", "crimson"]
-          // borderDash: [10, 5] // [dashLength, spaceLength]
+          data: [70, 10, 20],
+          backgroundColor: ["#FFE9AD", "#A1BFDF", "#F5C6CB"],
+          borderColor: ["#E4D099", "#829BB6", "#A68588"] 
         }
-        // {
-        //   label: "Weight",
-        //   data: [180, 179, 174, 174, 173, 169, 167, 166, 165, 161, 160],
-        //   fill: false,
-        //   borderColor: "red",
-        //   pointBackgroundColor: "red",
-        //   borderWidth: 1
-        // }
       ]
     };
   };
 
-  // const options = {
-  //   scales: {
-  //     yAxes: [
-  //       {
-  //         ticks: {
-  //           min: 140,
-  //           max: 200
-  //         }
-  //       }
-  //     ]
-  //   }
-  // };
+  const options = {
+    cutoutPercentage: 75,
+    tooltips: {
+      // callback runs before tool-tip is rendered
+      callbacks: {
+        label: (tooltipItem, data) => {
+          // grabs the index of the current tool-tip item
+          let dataIndex = tooltipItem.index;
+          // grabs the label for the current tool-tip
+          let dataLabel = data.labels[dataIndex];
+          // grabs the current data value for the dataset 
+          let dataVal = data.datasets[0].data[dataIndex];
+          // reformats tool-tip to be displayed as percentage
+          return ` ${dataLabel}: ${dataVal}%`;
+        }
+      }
+    },
+  };
 
-  return <Pie data={data} />;
+  return <Pie data={data} options={options}/>;
 };
 
 export default MacroProgress;

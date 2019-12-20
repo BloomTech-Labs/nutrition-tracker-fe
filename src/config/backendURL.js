7fced52a8b2f6b6cc25c19084098462df840348d
unknown <will.ediger@gmail.com>
2019-12-19 19:32:12 -0600

    refactored "backendURL" and moved hard coded production URL to separate constant, and added warning to developers not to edit the production URL unless they actually need to update the URL *for production*

--- src/config/backendURL.js
+++ src/config/backendURL.js
@@ -2,10 +2,18 @@
 
 // to set a custom backendURL, set the .env variable, `REACT_APP_backendURL`
 
-// do not make changes to this URL unless you are updating the master
-// backend URL address
+// *************
+// do not make changes to the URL unless you are updating the master
+// backend URL address!!
+// *************
+const productionURL = `https://nutri-journal.herokuapp.com`;
+//                     STOP! MOVE AWAY FROM THE COOKIE JAR!
+//                     ARE YOU SURE YOU SHOULD BE EDITING THIS VALUE?
 
-const backendURL =
-  process.env.REACT_APP_backendURL || `https://nutri-journal.herokuapp.com`;
+//                     If you need to point to a different URL for testing
+//                     or development, simply edit the `.env` file, eg set:
+//                     REACT_APP_backendURL=http://the-backend-you-want-to-use.com
+
+const backendURL = process.env.REACT_APP_backendURL || productionURL;
 
 module.exports = { backendURL };

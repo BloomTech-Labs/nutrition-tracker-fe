a113783727b16e20de82c87ca5c6df6ee61a0d2d
unknown <will.ediger@gmail.com>
2019-12-19 19:02:35 -0600

    prettier autoformat changes

--- src/store/actions/dailyLogActions.js
+++ src/store/actions/dailyLogActions.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 export const FETCH_NUTRITION_BUDGETS_START = "FETCH_NUTRITION_BUDGETS_START";
-export const FETCH_NUTRITION_BUDGETS_SUCCESS = "FETCH_NUTRITION_BUDGETS_SUCCESS";
-export const FETCH_NUTRITION_BUDGETS_FAILURE = "FETCH_NUTRITION_BUDGETS_FAILURE";
+export const FETCH_NUTRITION_BUDGETS_SUCCESS =
+  "FETCH_NUTRITION_BUDGETS_SUCCESS";
+export const FETCH_NUTRITION_BUDGETS_FAILURE =
+  "FETCH_NUTRITION_BUDGETS_FAILURE";
 export const FETCH_DAILY_LOG_START = "FETCH_DAILY_LOG_START";
 export const FETCH_DAILY_LOG_SUCCESS = "FETCH_DAILY_LOG_SUCCESS";
 export const FETCH_DAILY_LOG_FAILURE = "FETCH_DAILY_LOG_FAILURE";
@@ -11,21 +13,24 @@ export const UPDATE_CURRENT_TIMEZONE = "UPDATE_CURRENT_TIMEZONE";
 
 export const fetchNutritionBudgets = userID => dispatch => {
   dispatch({ type: FETCH_NUTRITION_BUDGETS_START });
-  axios.get(`https://nutri-journal.herokuapp.com/daily-log/${userID}/nutrition-budgets/`)
-    .then(({data}) => {
+  axios
+    .get(
+      `https://nutri-journal.herokuapp.com/daily-log/${userID}/nutrition-budgets/`
+    )
+    .then(({ data }) => {
       dispatch({
         type: FETCH_NUTRITION_BUDGETS_SUCCESS,
         payload: {
           caloricBudget: data.caloricBudget,
           fatBudget: data.fatBudget,
           carbBudget: data.carbBudget,
-          proteinBudget: data.proteinBudget,
+          proteinBudget: data.proteinBudget
         }
-      })
+      });
     })
     .catch(err => {
-      dispatch({type: FETCH_NUTRITION_BUDGETS_FAILURE})
-    })
+      dispatch({ type: FETCH_NUTRITION_BUDGETS_FAILURE });
+    });
 };
 
 export const fetchDailyLog = (userID, date, currentTimeZone) => dispatch => {
@@ -33,9 +38,11 @@ export const fetchDailyLog = (userID, date, currentTimeZone) => dispatch => {
 
   currentTimeZone = encodeURIComponent(currentTimeZone);
 
-  axios.get(`https://nutri-journal.herokuapp.com/daily-log/${userID}/${date}/${currentTimeZone}`)
-    .then(({data}) => {
-
+  axios
+    .get(
+      `https://nutri-journal.herokuapp.com/daily-log/${userID}/${date}/${currentTimeZone}`
+    )
+    .then(({ data }) => {
       dispatch({
         type: FETCH_DAILY_LOG_SUCCESS,
         payload: {
@@ -43,28 +50,27 @@ export const fetchDailyLog = (userID, date, currentTimeZone) => dispatch => {
             caloriesConsumed: data.caloriesConsumed,
             fatsConsumed: data.fatsConsumed,
             carbsConsumed: data.carbsConsumed,
-            proteinConsumed: data.proteinConsumed,
+            proteinConsumed: data.proteinConsumed
           },
           dailyLog: data.dailyLog
         }
-      })
+      });
     })
     .catch(err => {
-      dispatch({type: FETCH_DAILY_LOG_FAILURE})
-    })
+      dispatch({ type: FETCH_DAILY_LOG_FAILURE });
+    });
 };
 
 export const updateCurrentDate = currentDate => dispatch => {
   dispatch({
     type: UPDATE_CURRENT_DATE,
     payload: currentDate
-  })
-}
+  });
+};
 
 export const updateCurrentTimeZone = currentTimeZone => dispatch => {
   dispatch({
     type: UPDATE_CURRENT_TIMEZONE,
     payload: currentTimeZone
-  })
-}
-
+  });
+};

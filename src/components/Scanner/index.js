581fece3f689aa655d7cda1d1bb8b56c002c4dfe
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-12-26 03:17:01 -0800

    Pivoting to use quagga library. Have implemented livestream of webcam to display on screen. Will need to direct that stream to quagga for decoding

--- src/components/Scanner/index.js
+++ src/components/Scanner/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import decoderFile from "./decoder.js";
 
 //import QRReader from "./qrscan";
 
@@ -12,12 +13,30 @@ window.isMediaStreamAPISupported =
   "enumerateDevices" in navigator.mediaDevices;
 window.noCameraPermission = false;
 
+// helps us set up our web worker, which we'll use to communicate with the compiled decoder file
+// https://github.com/fullstackio/awesome-fullstack-tutorials/blob/master/react/guide-to-web-workers-in-react/react-worker/src/worker.js
+class WebWorker {
+    constructor(worker) {
+      const code = worker.toString();
+      const blob = new Blob(["(" + code + ")()"]);
+      return new Worker(URL.createObjectURL(blob));
+    }
+  }
+
 const Scanner = () => {
-  window.isMediaStreamAPISupported = navigator && navigator.mediaDevices && 'enumerateDevices' in navigator.mediaDevices;
+  window.isMediaStreamAPISupported =
+    navigator &&
+    navigator.mediaDevices &&
+    "enumerateDevices" in navigator.mediaDevices;
   let canvasRef = useRef(null);
   let webcamRef = useRef(null);
   let forSelectedPhotos = false;
   let streaming = false;
+  let QRActive = false;
+  let canvas = null,
+        ctx = null;
+  const decoder = new WebWorker(decoderFile);
+
 
   function showErrorMsg() {
     window.noCameraPermission = true;
@@ -29,14 +48,16 @@ const Scanner = () => {
       .getUserMedia(constraints)
       .then(function(stream) {
         webcamRef.current.srcObject = stream;
-        webcamRef.current.setAttribute('playsinline', true);
-        webcamRef.current.setAttribute('controls', true);
-        setTimeout(() => {
-          webcamRef.current.removeAttribute('controls');
-        });
+        //webcamRef.current.setAttribute("controls", true);
+        console.log("heyo", webcamRef.current);
+        webcamRef.current.setAttribute("playsinline", true);
+        webcamRef.current.setAttribute("autoplay", true);
+        /*setTimeout(() => {
+          webcamRef.current.removeAttribute("controls");
+        });*/ // Trying to figure out why we need this removeAttributes...
       })
       .catch(function(err) {
-        console.log('Error occurred ', err);
+        console.log("Error occurred ", err);
         showErrorMsg();
       });
   }
@@ -46,8 +67,8 @@ const Scanner = () => {
       .enumerateDevices()
       .then(function(devices) {
         let device = devices.filter(function(device) {
-          let deviceLabel = device.label.split(',')[1];
-          if (device.kind == 'videoinput') {
+          let deviceLabel = device.label.split(",")[1];
+          if (device.kind == "videoinput") {
             return device;
           }
         });
@@ -64,7 +85,7 @@ const Scanner = () => {
           };
 
           if (window.iOS) {
-            constraints.video.facingMode = 'environment';
+            constraints.video.facingMode = "environment";
           }
           startCapture(constraints);
         } else if (device.length) {
@@ -78,7 +99,7 @@ const Scanner = () => {
           };
 
           if (window.iOS) {
-            constraints.video.facingMode = 'environment';
+            constraints.video.facingMode = "environment";
           }
 
           startCapture(constraints);
@@ -88,56 +109,109 @@ const Scanner = () => {
       })
       .catch(function(error) {
         showErrorMsg();
-        console.error('Error occurred : ', error);
+        console.error("Error occurred : ", error);
       });
   }
+
+  const scan = (callback, forSelectedPhotos) => {
+    QRActive = true;
+
+    function onDecoderMessage(event) {
+      if (event.data.length > 0) {
+        var qrid = event.data[0][2];
+        QRActive = false;
+        callback(qrid);
+      }
+      setTimeout(newDecoderFrame, 0);
+    }
+
+    decoder.onmessage = onDecoderMessage;
+
+    /*setTimeout(() => {
+      setPhotoSourceToScan(forSelectedPhotos);
+    });*/ // !!! FIX !!!!
+
+    // Start QR-decoder
+    function newDecoderFrame() {
+      if (!QRActive) return;
+      try {
+        ctx.drawImage(
+          webcamRef,
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        );
+        console.log("!!!! hey");
+        var imgData = ctx.getImageData(
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        );
+
+        if (imgData.data) {
+          decoder.postMessage(imgData);
+        }
+      } catch (e) {
+        // Try-Catch to circumvent Firefox Bug #879717
+        if (e.name == "NS_ERROR_NOT_AVAILABLE") setTimeout(newDecoderFrame, 0);
+      }
+    }
+    newDecoderFrame();
+  };
 
   /*useEffect(() => {
     QRReader.init(); // init scanner
     QRReader.ctx = canvasRef.current.getContext("2d")
   }, []);*/
 
-
   return (
     <>
       <div className="app-layout-content">
-                {/* Original code use querySelectors but since we're using React, we'll use ref to  emulate the behavior */}
-                {window.isMediaStreamAPISupported && !forSelectedPhotos
-                ? <video autoplay ref={webcamRef} onPlay={
-                    (e) => {
-                        e.stopPropagation(); // calling this because in original code, `false` was passed to capture arg of addEventListener
-                        // https://reactjs.org/docs/events.html
-                        // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
-                        if (!streaming) {
-                            streaming = true;
-                        }
-                    }
-                }/>
-                : <img id="frame" src="" alt="" ef={webcamRef}/>}
-                <div className="app-dialog app-dialog-hide">
-                    <div className="app-dialog-content">
-                        <h2>QR Code</h2>
-                        <input type="text" id="result" />
-                    </div>
-                    <div cassName="app-dialog-actions">
-                        <button className="app-dialog-open">Open</button>
-                        <button className="app-dialog-close">Close</button>
-                    </div>
-                </div>
-                This is a scanner!!
-            </div>
+        {/* Original code use querySelectors but since we're using React, we'll use ref to  emulate the behavior */}
+        {window.isMediaStreamAPISupported && !forSelectedPhotos ? (
+          <video
+            autoplay={true}
+            ref={webcamRef}
+            onPlay={e => {
+              e.stopPropagation(); // calling this because in original code, `false` was passed to capture arg of addEventListener
+              // https://reactjs.org/docs/events.html
+              // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+              if (!streaming) {
+                streaming = true;
+              }
+            }}
+          />
+        ) : (
+          <img id="frame" src="" alt="" ref={webcamRef} />
+        )}
+        <div className="app-dialog app-dialog-hide">
+          <div className="app-dialog-content">
+            <h2>QR Code</h2>
+            <input type="text" id="result" />
+          </div>
+          <div cassName="app-dialog-actions">
+            <button className="app-dialog-open">Open</button>
+            <button className="app-dialog-close">Close</button>
+          </div>
+        </div>
+        This is a scanner!!
+      </div>
 
       <canvas
         ref={canvasRef}
         width={window.innerWidth}
         height={window.innerHeight}
         onClick={e => {
-          const canvas = canvasRef.current,
+          canvas = canvasRef.current;
             ctx = canvas.getContext("2d");
         }}
       />
 
-      <button className="app-select-photos" >Simulates the option to select a photo from storage </button>
+      <button className="app-select-photos">
+        Simulates the option to select a photo from storage{" "}
+      </button>
     </>
   );
 };

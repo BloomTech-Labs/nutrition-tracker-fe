285eacfb0b2f9491f39c00dcd1935c890d5a27f3
Dave Vazquez <dmvaz.li@gmail.com>
2020-01-01 11:58:30 -0500

    refactors WeightProgress chart

--- src/components/ProgressReports/components/charts/WeightProgress.js
+++ src/components/ProgressReports/components/charts/WeightProgress.js
@@ -1,86 +1,66 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { Line } from "react-chartjs-2";
-
-/*
-
-  From End Point:
-  
-    Arrays:
-      range of dates
-
-      x = applicable_date
-      y = 
-      180 + -2 * (x - Date(10/4))
-
-*/
-
-/*
-  160 - 180 = -20
-  10/14 - 10/4 = 10
-
-  -20/10 = -2
-*/
-
-/*
-  start weight
-  start date
-  weekly_weight_loss_goal / 7
-*/
-
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { getWeightProgress } from "../../../../store/actions/progressOverviewActions";
+import useConfigChart from "../custom-hooks/useConfigChart";
 
 const WeightProgress = () => {
-  const data = () => {
-    return {
-      labels: [
-        "10/04", // 180
-        "10/05", // 180 + (- 2 * 1)
-        "10/06", // 180 + (- 2 * 2)
-        "10/07", // 180 + (- 2 * 3)
-        "10/08", // 180 + (- 2 * 4)
-        "10/09", // 180 - 2 * 5
-        "10/10", // 180 - 2 * 6
-        "10/11", // 180 - 2 * 7
-        "10/12", // 180 - 2 * 8
-        "10/13", // 180 - 2 * 9
-        "10/14", // 180 - 2 * 10
-        "10/15"  // 180 - 2 * 11
-      ],
-      datasets: [
-        {
-          label: "Target",
-          data: [180, 178, 176, 174, 172, 170, 168, 166, 164, 162, 160],
-          fill: false,
-          borderColor: "black",
-          borderDash: [10, 5] // [dashLength, spaceLength]
-        },
-        {
-          label: "Weight",
-          data: [180, 179, 174, 174, 173, 169, 167, 166, 165, 161, 160],
-          fill: false,
-          borderColor: "red",
-          pointBackgroundColor: "red",
-          borderWidth: 1
-        }
-      ]
-    };
-  };
+  const dispatch = useDispatch();
+  // variable is true when the user's firebase profile is loaded
+  const isLoaded = useSelector(state => state.firebase.profile.isLoaded);
+  const progressPeriod = useSelector(state => state.progressPeriod);
 
-  const options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            min: 140,
-            max: 200
-          }
-        }
-      ]
+  const {
+    weightsOverTime,
+    getWeightProgressStart,
+    getWeightProgressSuccess,
+    getWeightProgressFailure
+  } = useSelector(state => state.progressOverview);
+
+  // initializes the Line Chart's data and options
+  const { data, options, chartConfigured } = useConfigChart({
+    weightsOverTime
+  });
+
+  useEffect(() => {
+    if (isLoaded) {
+      const { time_zone, start_date, end_date, period } = progressPeriod;
+      dispatch(
+        getWeightProgress(
+          /*firebaseID*/ "dave",
+          time_zone,
+          start_date,
+          end_date,
+          period
+        )
+      );
     }
-  };
-  
+  }, [isLoaded, progressPeriod, dispatch]);
+
   return (
-    <Line data={data} options={options} />
-  )
-}
+    <ChartWrapper>
+      {getWeightProgressStart && <h3>Loading...</h3>}
+      {getWeightProgressFailure && (
+        <h3>Could not retrive data. Try again later.</h3>
+      )}
+      {getWeightProgressSuccess && chartConfigured && (
+        <>
+          <h3>Actual Weight vs. Target</h3>
+          <Line data={data} options={options} height={200} />
+        </>
+      )}
+    </ChartWrapper>
+  );
+};
+
+const ChartWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+  height: 280px;
+`;
 
 export default WeightProgress;

2bea491d51d060c4b30c0b2f1837b67e64667699
Dave Vazquez <dmvaz.li@gmail.com>
2020-01-01 20:44:11 -0500

    adds state for getting average macros consumed

--- src/store/reducers/progressOverviewReducer.js
+++ src/store/reducers/progressOverviewReducer.js
@@ -1,4 +1,7 @@
 import {
+  GET_AVERAGE_MACROS_FAILURE,
+  GET_AVERAGE_MACROS_START,
+  GET_AVERAGE_MACROS_SUCCESS,
   GET_WEIGHT_PROGRESS_FAILURE,
   GET_WEIGHT_PROGRESS_START,
   GET_WEIGHT_PROGRESS_SUCCESS
@@ -6,17 +9,28 @@ import {
 
 const initialState = {
   weightsOverTime: [],
+  averageMacros: {},
   getWeightProgressStart: false,
   getWeightProgressSuccess: false,
-  getWeightProgressFailure: false
+  getWeightProgressFailure: false,
+  getAverageMacrosStart: false,
+  getAverageMacrosSuccess: false,
+  getAverageMacrosFailure: false
 };
 
 export const progressOverviewReducer = (state = initialState, action) => {
-  const { weightsOverTime } = action.payload ? action.payload : initialState;
+  const { weightsOverTime } = action.payload
+    ? action.payload
+    : initialState.weightsOverTime;
+
+  const { averageMacros } = action.payload
+    ? action.payload
+    : initialState.averageMacros;
 
   switch (action.type) {
     case GET_WEIGHT_PROGRESS_START: {
       return {
+        ...state,
         weightsOverTime,
         getWeightProgressStart: true,
         getWeightProgressSuccess: false,
@@ -26,6 +40,7 @@ export const progressOverviewReducer = (state = initialState, action) => {
 
     case GET_WEIGHT_PROGRESS_SUCCESS: {
       return {
+        ...state,
         weightsOverTime,
         getWeightProgressStart: false,
         getWeightProgressSuccess: true,
@@ -35,10 +50,41 @@ export const progressOverviewReducer = (state = initialState, action) => {
 
     case GET_WEIGHT_PROGRESS_FAILURE: {
       return {
+        ...state,
         weightsOverTime,
         getWeightProgressStart: false,
         getWeightProgressSuccess: false,
         getWeightProgressFailure: true
+      };
+    }
+
+    case GET_AVERAGE_MACROS_START: {
+      return {
+        ...state,
+        averageMacros,
+        getAverageMacrosStart: true,
+        getAverageMacrosSuccess: false,
+        getAverageMacrosFailure: false
+      };
+    }
+
+    case GET_AVERAGE_MACROS_SUCCESS: {
+      return {
+        ...state,
+        averageMacros,
+        getAverageMacrosStart: false,
+        getAverageMacrosSuccess: true,
+        getAverageMacrosFailure: false
+      };
+    }
+
+    case GET_AVERAGE_MACROS_FAILURE: {
+      return {
+        ...state,
+        averageMacros,
+        getAverageMacrosStart: false,
+        getAverageMacrosSuccess: false,
+        getAverageMacrosFailure: true
       };
     }
 

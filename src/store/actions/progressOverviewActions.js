574b1b50ce32ee93650ac8296b44d91bcabb7c97
Dave Vazquez <dmvaz.li@gmail.com>
2020-01-01 20:43:55 -0500

    adds action for getting average macros consumed

--- src/store/actions/progressOverviewActions.js
+++ src/store/actions/progressOverviewActions.js
@@ -4,6 +4,9 @@ import { backendURL } from "../../config/backendURL.js";
 export const GET_WEIGHT_PROGRESS_START = "GET_WEIGHT_PROGRESS_START";
 export const GET_WEIGHT_PROGRESS_SUCCESS = "GET_WEIGHT_PROGRESS_SUCCESS";
 export const GET_WEIGHT_PROGRESS_FAILURE = "GET_WEIGHT_PROGRESS_FAILURE";
+export const GET_AVERAGE_MACROS_START = "GET_AVERAGE_MACROS_START";
+export const GET_AVERAGE_MACROS_SUCCESS = "GET_AVERAGE_MACROS_SUCCESS";
+export const GET_AVERAGE_MACROS_FAILURE = "GET_AVERAGE_MACROS_FAILURE";
 
 export const getWeightProgress = (
   user_id,
@@ -30,6 +33,28 @@ export const getWeightProgress = (
     .catch(err => {
       dispatch({
         type: GET_WEIGHT_PROGRESS_FAILURE,
+        err: err
+      });
+    });
+};
+
+export const getAverageMacrosConsumed = (user_id, start_date) => dispatch => {
+  dispatch({ type: GET_AVERAGE_MACROS_START });
+  axios
+    .post(`${backendURL}/progress-reports/${user_id}/macros/average`, {
+      start_date
+    })
+    .then(({ data }) => {
+      dispatch({
+        type: GET_AVERAGE_MACROS_SUCCESS,
+        payload: {
+          averageMacros: data.averageMacros
+        }
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_AVERAGE_MACROS_FAILURE,
         err: err
       });
     });

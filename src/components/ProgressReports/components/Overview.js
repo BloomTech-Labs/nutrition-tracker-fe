9fe21c32b2ad6151973901ee8a2ab454e8300bb7
Dave Vazquez <dmvaz.li@gmail.com>
2020-01-01 20:42:29 -0500

    adds state to Overview

--- src/components/ProgressReports/components/Overview.js
+++ src/components/ProgressReports/components/Overview.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getWeightProgress } from "../../../store/actions/progressOverviewActions";
+import { getAverageMacrosConsumed } from "../../../store/actions/progressOverviewActions";
 import { Col, Row } from "../../Global/styled";
 import MacroProgress from "./charts/MacroProgress";
 import WeightProgress from "./charts/WeightProgress";
 
 const Overview = () => {
+  const dispatch = useDispatch();
+
+  const isLoaded = useSelector(state => state.firebase.profile.isLoaded);
+  const firebaseID = useSelector(state => state.firebase.auth.uid);
+  const progressPeriod = useSelector(state => state.progressPeriod);
+
+  const { averageMacros, weightsOverTime } = useSelector(
+    state => state.progressOverview
+  );
+
+  useEffect(() => {
+    if (isLoaded) {
+      const { time_zone, start_date, end_date, period } = progressPeriod;
+      dispatch(
+        getWeightProgress(
+          /*firebaseID*/ "dave",
+          time_zone,
+          start_date,
+          end_date,
+          period
+        )
+      );
+
+      dispatch(getAverageMacrosConsumed(/*firebaseID*/ "12345", start_date));
+    }
+  }, [isLoaded, progressPeriod, dispatch]);
+
   return (
     <>
-      {/* <Row>
-        <Col>
-          <MacroProgress />
-        </Col>
-      </Row> */}
       <Row>
         <Col>
-          <WeightProgress />
+          <MacroProgress averageMacros={averageMacros} />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <WeightProgress weightsOverTime={weightsOverTime} />
         </Col>
       </Row>
     </>

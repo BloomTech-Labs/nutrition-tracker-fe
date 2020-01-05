d1a4743b083b9a2bf10ad8ff1be38d94a29028ad
JimmyMcBride <mcbride967@gmail.com>
2020-01-04 21:33:07 -0600

    updated macro redux to be more dry

--- src/store/actions/macroProgress.js
+++ src/store/actions/macroProgress.js
@@ -1,83 +1,34 @@
 import axios from "axios";
+import { backendURL } from "../../config/backendURL.js";
 
-export const WEEKLY_MACRO_PROGRESS_START = "WEEKLY_MACRO_PROGRESS_START",
-  WEEKLY_MACRO_PROGRESS_SUCCESS = "WEEKLY_MACRO_PROGRESS_SUCCESS",
-  WEEKLY_MACRO_PROGRESS_FAILURE = "WEEKLY_MACRO_PROGRESS_FAILURE",
-  MONTHLY_MACRO_PROGRESS_START = "MONTHLY_MACRO_PROGRESS_START",
-  MONTHLY_MACRO_PROGRESS_SUCCESS = "MONTHLY_MACRO_PROGRESS_SUCCESS",
-  MONTHLY_MACRO_PROGRESS_FAILURE = "MONTHLY_MACRO_PROGRESS_FAILURE",
-  QUARTERLY_MACRO_PROGRESS_START = "QUARTERLY_MACRO_PROGRESS_START",
-  QUARTERLY_MACRO_PROGRESS_SUCCESS = "QUARTERLY_MACRO_PROGRESS_SUCCESS",
-  QUARTERLY_MACRO_PROGRESS_FAILURE = "QUARTERLY_MACRO_PROGRESS_FAILURE",
-  BIANNUAL_MACRO_PROGRESS_START = "BIANNUAL_MACRO_PROGRESS_START",
-  BIANNUAL_MACRO_PROGRESS_SUCCESS = "BIANNUAL_MACRO_PROGRESS_SUCCESS",
-  BIANNUAL_MACRO_PROGRESS_FAILURE = "BIANNUAL_MACRO_PROGRESS_FAILURE";
-
-export const getWeeklyMacroProgress = user_id => dispatch => {
-  dispatch({ type: WEEKLY_MACRO_PROGRESS_START });
+export const getMacroProgress = (
+  user_id,
+  time_zone,
+  start_date,
+  end_date,
+  period
+) => dispatch => {
+  dispatch({ type: GET_MACROS_PROGRESS_START });
   axios
-    .get(`http://localhost:4000/progress/${user_id}`)
-    .then(res =>
-      dispatch({ type: WEEKLY_MACRO_PROGRESS_SUCCESS, payload: res.data })
-    )
-    .catch(err =>
+    .post(`${backendURL}/progress-reports/${user_id}/weight/${period}`, {
+      time_zone,
+      start_date,
+      end_date
+    })
+    .then(({ data }) => {
       dispatch({
-        type: WEEKLY_MACRO_PROGRESS_FAILURE,
-        payload: err.message
-      })
-    );
-};
-
-export const getMonthlyMacroProgress = user_id => dispatch => {
-  dispatch({ type: MONTHLY_MACRO_PROGRESS_START });
-  axios
-    .get(`http://localhost:4000/progress/monthly-actuals/${user_id}`)
-    .then(res =>
+        type: GET_MACROS_PROGRESS_SUCCESS,
+        payload: {
+          fats: data.fats,
+          carbs: data.carbs,
+          protein: data.protein
+        }
+      });
+    })
+    .catch(err => {
       dispatch({
-        type: MONTHLY_MACRO_PROGRESS_SUCCESS,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: MONTHLY_MACRO_PROGRESS_FAILURE,
-        payload: err.message
-      })
-    );
-};
-
-export const getQuarterlyMacroProgress = user_id => dispatch => {
-  dispatch({ type: QUARTERLY_MACRO_PROGRESS_START });
-  axios
-    .get(`http://localhost:4000/progress/quarterly-actuals/${user_id}`)
-    .then(res =>
-      dispatch({
-        type: QUARTERLY_MACRO_PROGRESS_SUCCESS,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: QUARTERLY_MACRO_PROGRESS_FAILURE,
-        payload: err.message
-      })
-    );
-};
-
-export const getBiannualMacroProgress = user_id => dispatch => {
-  dispatch({ type: BIANNUAL_MACRO_PROGRESS_START });
-  axios
-    .get(`http://localhost:4000/progress/biannual-actuals/${user_id}`)
-    .then(res =>
-      dispatch({
-        type: BIANNUAL_MACRO_PROGRESS_SUCCESS,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: BIANNUAL_MACRO_PROGRESS_FAILURE,
-        payload: err.message
-      })
-    );
+        type: GET_MACROS_PROGRESS_FAILURE,
+        err: err
+      });
+    });
 };
--- src/store/reducers/macroProgress.js
+++ src/store/reducers/macroProgress.js
@@ -1,116 +1,41 @@
 import {
-  WEEKLY_MACRO_PROGRESS_START,
-  WEEKLY_MACRO_PROGRESS_SUCCESS,
-  WEEKLY_MACRO_PROGRESS_FAILURE,
-  MONTHLY_MACRO_PROGRESS_START,
-  MONTHLY_MACRO_PROGRESS_SUCCESS,
-  MONTHLY_MACRO_PROGRESS_FAILURE,
-  QUARTERLY_MACRO_PROGRESS_START,
-  QUARTERLY_MACRO_PROGRESS_SUCCESS,
-  QUARTERLY_MACRO_PROGRESS_FAILURE,
-  BIANNUAL_MACRO_PROGRESS_START,
-  BIANNUAL_MACRO_PROGRESS_SUCCESS,
-  BIANNUAL_MACRO_PROGRESS_FAILURE
+  GET_MACROS_PROGRESS_START,
+  GET_MACROS_PROGRESS_SUCCESS,
+  GET_MACROS_PROGRESS_FAILURE
 } from "../actions/macroProgress";
 
 const initialState = {
   error: "",
-  weekly_fats: [],
-  weekly_carbs: [],
-  weekly_protein: [],
-  monthly_fats: [],
-  monthly_carbs: [],
-  monthly_protein: [],
-  quarterly_fats: [],
-  quarterly_carbs: [],
-  quarterly_protein: [],
-  biannual_fats: [],
-  biannual_carbs: [],
-  biannual_protein: [],
+  actual_fats: [],
+  actual_carbs: [],
+  actual_protein: [],
+  target_fats: [],
+  target_carbs: [],
+  target_protein: [],
+  labels: [],
   getting: false
 };
 
 export const macroProgressReducer = (state = initialState, action) => {
   switch (action.type) {
-    // WEEKLY
-
-    case WEEKLY_MACRO_PROGRESS_START:
+    case GET_MACROS_PROGRESS_START:
       return {
         ...state,
         getting: true
       };
-    case WEEKLY_MACRO_PROGRESS_SUCCESS:
+    case GET_MACROS_PROGRESS_SUCCESS:
       return {
         ...state,
-        weekly_fats: action.payload.fats,
-        weekly_carbs: action.payload.carbs,
-        weekly_protein: action.payload.protein,
+        actual_fats: action.payload.actual_fats,
+        actual_carbs: action.payload.actual_carbs,
+        actual_protein: action.payload.actual_protein,
+        target_fats: action.payload.target_fats,
+        target_carbs: action.payload.target_carbs,
+        target_protein: action.payload.target_protein,
+        labels: action.payload.labels,
         getting: false
       };
-    case WEEKLY_MACRO_PROGRESS_FAILURE:
-      return {
-        ...state,
-        getting: false
-      };
-
-    // MONTHLY
-
-    case MONTHLY_MACRO_PROGRESS_START:
-      return {
-        ...state,
-        getting: true
-      };
-    case MONTHLY_MACRO_PROGRESS_SUCCESS:
-      return {
-        ...state,
-        monthly_fats: action.payload.fats,
-        monthly_carbs: action.payload.carbs,
-        monthly_protein: action.payload.protein,
-        getting: false
-      };
-    case MONTHLY_MACRO_PROGRESS_FAILURE:
-      return {
-        ...state,
-        getting: false
-      };
-
-    // QUARTERLY
-
-    case QUARTERLY_MACRO_PROGRESS_START:
-      return {
-        ...state,
-        getting: true
-      };
-    case QUARTERLY_MACRO_PROGRESS_SUCCESS:
-      return {
-        ...state,
-        quarterly_fats: action.payload.fats,
-        quarterly_carbs: action.payload.carbs,
-        quarterly_protein: action.payload.protein,
-        getting: false
-      };
-    case QUARTERLY_MACRO_PROGRESS_FAILURE:
-      return {
-        ...state,
-        getting: false
-      };
-
-    // BIANNUAL
-
-    case BIANNUAL_MACRO_PROGRESS_START:
-      return {
-        ...state,
-        getting: true
-      };
-    case BIANNUAL_MACRO_PROGRESS_SUCCESS:
-      return {
-        ...state,
-        biannual_fats: action.payload.fats,
-        biannual_carbs: action.payload.carbs,
-        biannual_protein: action.payload.protein,
-        getting: false
-      };
-    case BIANNUAL_MACRO_PROGRESS_FAILURE:
+    case GET_MACROS_PROGRESS_FAILURE:
       return {
         ...state,
         getting: false

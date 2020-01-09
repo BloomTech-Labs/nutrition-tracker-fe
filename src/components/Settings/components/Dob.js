9e3220e4374a7e3247a2becc1648134946970869
Chaz Rowe <chazr_88@hotmail.com>
2020-01-08 21:33:53 -0500

    checking if fixed

---
+++ src/components/Settings/components/Dob.js
@@ -0,0 +1,81 @@
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  ListGroupItem,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader
+} from "reactstrap";
+import { CalendarSVG } from "../../Global/icons";
+import { InputGroupWithIcon } from "../../Global/styled/index";
+import { ListStyle } from "../styles";
+
+const Dob = props => {
+  const [modal, setModal] = useState(false);
+
+  const [dob, setDob] = useState("");
+
+  useEffect(() => {
+    setDob(props.data.dob);
+  }, [props.data.dob]);
+
+  const toggle = () => setModal(!modal);
+  return (
+    <div>
+      <ListGroupItem onClick={toggle} style={ListStyle}>
+        <div>Date Of Birth</div>
+        {/* Formats date to be displayed to the user */}
+        <div>{moment(dob).utc().format("MMM DD YY")}</div>
+      </ListGroupItem>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader>Date Of Birth</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="dob">Date Of Birth</Label>
+              <InputGroupWithIcon
+                type="date"
+                name="date_of_birth"
+                icon={CalendarSVG}
+                placeholder="MM/DD/YYYY"
+                handleChange={e => setDob(e.target.value)}
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => {
+              toggle();
+              props.updateUser({ dob: toIsoDateString(dob) });
+            }}
+          >
+            Update
+          </Button>{" "}
+          <Button
+            color="secondary"
+            onClick={() => {
+              toggle();
+              setDob(props.data.dob);
+            }}
+          >
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+//Formats the date how the DB needs it.
+function toIsoDateString(date) {
+  return new Date(date).toISOString();
+}
+
+export default Dob;
---
+++ src/components/Settings/components/Email.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { ListStyle } from "../styles";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ListGroupItem,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+
+const Email = props => {
+  const [modal, setModal] = useState(false);
+
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    setEmail(props.data.email);
+  }, [props.data.email]);
+
+  const toggle = () => setModal(!modal);
+  return (
+    <div>
+      <ListGroupItem onClick={toggle} style={ListStyle}>
+        <div>Email</div>
+        <div>{email}</div>
+      </ListGroupItem>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader>Email</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                type="text"
+                name="email"
+                id="email"
+                value={email || ""}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => {
+              toggle();
+              props.updateUser({ email });
+            }}
+          >
+            Update
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default Email;

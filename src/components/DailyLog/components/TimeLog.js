a1942b06b7a844006bfc5e6fce351ed32cc02463
Dave Vazquez <dmvaz.li@gmail.com>
2020-01-09 20:49:54 -0500

    formatting

--- src/components/DailyLog/components/TimeLog.js
+++ src/components/DailyLog/components/TimeLog.js
@@ -6,36 +6,35 @@ import { Col, H4, Row } from "../../Global/styled";
 const TimeLog = ({ dailyLog }) => {
   return (
     <div>
-      {dailyLog.length === 0 &&
+      {dailyLog.length === 0 && (
         <Row>
           <Col height="225px" justify="center" align="center">
             <H4>Search for a food item to begin logging!</H4>
           </Col>
-        </Row>}
+        </Row>
+      )}
       {dailyLog &&
-        dailyLog.map((interval, i) =>
+        dailyLog.map((interval, i) => (
           <Row key={i}>
             <Col>
               <Table>
                 <tbody>
-                  {interval.map((log, i) =>
+                  {interval.map((log, i) => (
                     <tr key={i}>
-                      {log.firstGroupLog
-                        ? log.hasTimeZoneDifference
-                          ? <TimeHeader twoTimeZones>
-                              {log.intervalStartHere}
-                              <br />
-                              <span>
-                                {log.intervalStartThere}
-                              </span>
-                            </TimeHeader>
-                          : <TimeHeader>
-                              {log.intervalStart}
-                            </TimeHeader>
-                        : <TimeHeader />}
-                      <FoodName className="food-name">
-                        {log.foodName}
-                      </FoodName>
+                      {log.firstGroupLog ? (
+                        log.hasTimeZoneDifference ? (
+                          <TimeHeader twoTimeZones>
+                            {log.intervalStartHere}
+                            <br />
+                            <span>{log.intervalStartThere}</span>
+                          </TimeHeader>
+                        ) : (
+                          <TimeHeader>{log.intervalStart}</TimeHeader>
+                        )
+                      ) : (
+                        <TimeHeader />
+                      )}
+                      <FoodName className="food-name">{log.foodName}</FoodName>
                       <ServingName className="serving">
                         {log.servingDescription}
                       </ServingName>
@@ -43,19 +42,19 @@ const TimeLog = ({ dailyLog }) => {
                         {Math.trunc(log.quantity)}
                       </Quantity>
                     </tr>
-                  )}
+                  ))}
                 </tbody>
               </Table>
             </Col>
           </Row>
-        )}
+        ))}
     </div>
   );
 };
 
 const Table = styled(BS_Table)`
   font-size: 1.2rem;
-  margin:0;
+  margin: 0;
 
   tr {
     &:hover {
@@ -72,7 +71,7 @@ const Table = styled(BS_Table)`
 
 const TimeHeader = styled.th`
   font-size: 1.4rem;
-  text-align: right;
+  text-align: left;
   border: none;
   width: 25%;
 
@@ -85,9 +84,15 @@ const TimeHeader = styled.th`
   }
 `;
 
-const FoodName = styled.td`text-align: left;`;
+const FoodName = styled.td`
+  text-align: left;
+  font-size: 1.4rem;
+`;
 
-const ServingName = styled.td`text-align: right;`;
+const ServingName = styled.td`
+  text-align: right;
+  font-size: 1.4rem;
+`;
 
 const Quantity = styled.td`
   text-align: right;

4220fdc493e13f320dd4c5991c36fe010f9e4f6e
Dave Vazquez <dmvaz.li@gmail.com>
2020-01-09 20:51:42 -0500

    initial commit

---
+++ src/components/ProgressReports/components/charts/LineChart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+export default function LineChart({ actuals, goals, labels, info, options }) {
+  const data = () => {
+    return {
+      labels: labels,
+      datasets: [
+        {
+          label: info.title_1,
+          data: actuals,
+          fill: false,
+          borderColor: info.color_1,
+          pointBackgroundColor: info.color_2
+        },
+        {
+          label: info.title_2,
+          data: goals,
+          fill: false,
+          borderColor: "#444",
+          pointBackgroundColor: "gray"
+        }
+      ]
+    };
+  };
+
+  return <Line data={data} options={options} />;
+}
